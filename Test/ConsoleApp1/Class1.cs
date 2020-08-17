44e08aea8fb17c0035a857b12332f6d85b2e2afe
q361431349 <40018342+q361431349@users.noreply.github.com>
2020-08-17 17:50:58 +0800

    Add files via upload

---
+++ Test/ConsoleApp1/Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Class1
+    {
+        private string firstName;
+        public string FirstName
+        {
+            get => firstName;
+            set
+            {
+                firstName = value;
+                fullName = null;
+            }
+        }
+
+        private string lastName;
+        public string LastName
+        {
+            get => lastName;
+            set
+            {
+                lastName = value;
+                fullName = null;
+            }
+        }
+
+        private string fullName;
+        public string FullName
+        {
+            get
+            {
+                if (fullName == null)
+                    fullName = $"{FirstName} {LastName}";
+                return fullName;
+            }
+        }
+
+        [field: NonSerialized]
+        public int Id { get; set; }
+    }
+}
---
+++ Test/ConsoleApp1/Motorcycle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    abstract class Motorcycle
+    {
+
+        public void StartEngine() { }
+
+        public void AddGas(int gallons) { }
+
+
+        public virtual int Drive(int miles, int speed) { return 1; }
+
+
+        public virtual int Drive(TimeSpan time, int speed) { return 0; }
+
+
+        public abstract double GetTopSpeed();
+        public virtual void A() { }
+    }
+}
