c5019f153fb767672feb76bca12c7df5ff07fe1e
q361431349 <40018342+q361431349@users.noreply.github.com>
2020-08-18 17:21:27 +0800

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/GenericList.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class GenericList<T>
+    {
+        public void Add(T input) {
+            Console.WriteLine(input);
+        }
+        private class Node
+        {
+            // T used in non-generic constructor.
+            public Node(T t)
+            {
+                Next = null;
+                Data = t;
+            }
+
+            public Node Next { get; set; }
+
+            // T as return type of property.
+            public T Data { get; set; }
+        }
+
+        private Node head;
+
+        // constructor
+        public GenericList()
+        {
+            head = null;
+        }
+
+        // T as method parameter type:
+        public void AddHead(T t)
+        {
+            Node n = new Node(t);
+            n.Next = head;
+            head = n;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            Node current = head;
+
+            while (current != null)
+            {
+                yield return current.Data;
+                current = current.Next;
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Mandelbrot.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Mandelbrot
+    {
+        readonly private int maxIterations;
+
+        public Mandelbrot(int maxIterations)
+        {
+            this.maxIterations = maxIterations;
+        }
+
+        public int this[double x, double y]
+        {
+            get
+            {
+                var iterations = 0;
+                var x0 = x;
+                var y0 = y;
+
+                while ((x * x + y * y < 4) &&
+                    (iterations < maxIterations))
+                {
+                    var newX = x * x - y * y + x0;
+                    y = 2 * x * y + y0;
+                    x = newX;
+                    iterations++;
+                }
+                return iterations;
+            }
+        }
+    }
+}
