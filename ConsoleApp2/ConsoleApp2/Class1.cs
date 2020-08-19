f50d436e7463747853f05663579e357676f99538
q361431349 <40018342+q361431349@users.noreply.github.com>
2020-08-19 17:26:47 +0800

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Class1.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    static class Class1
+    {
+        public static IEnumerable<T> Sample<T>(this IEnumerable<T> sourceSequence, int interval)
+        {
+            int index = 0;
+            foreach (T item in sourceSequence)
+            {
+                if (index++ % interval == 0)
+                    yield return item;
+            }
+        }
+    }
+}
