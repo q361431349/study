2ff6b1a94d0d64d7f52172a4f287f33940e79566
q361431349 <40018342+q361431349@users.noreply.github.com>
2020-08-20 09:15:55 +0800

    Add files via upload

---
+++ ConsoleApp2/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form2 form2 { get; set; }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //DownloadFile(textBox1.Text, textBox2.Text);
+            form2.Show();
+            form2.UpdateUI(textBox1.Text, textBox2.Text);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+          
+            form2 = new Form2(this);
+            form2.Show();
+
+        }
+
+        public void DownloadFile(string URL, string filename, ProgressBar prog)
+        {
+            try
+            {
+                HttpWebRequest Myrq = (HttpWebRequest)WebRequest.Create(URL);
+                HttpWebResponse myrp = (HttpWebResponse)Myrq.GetResponse();
+                long totalBytes = myrp.ContentLength;
+
+                if (prog != null)
+                {
+                    prog.Maximum = (int)totalBytes;
+                }
+
+                Stream st = myrp.GetResponseStream();
+                Stream so = new FileStream(filename, FileMode.Create);
+                long totalDownloadedByte = 0;
+                byte[] by = new byte[1024];
+                int osize = st.Read(by, 0, by.Length);
+                while (osize > 0)
+                {
+                    totalDownloadedByte = osize + totalDownloadedByte;
+                    Application.DoEvents();
+                    so.Write(by, 0, osize);
+
+                    if (prog != null)
+                    {
+                        prog.Value = (int)totalDownloadedByte;
+                    }
+                    osize = st.Read(by, 0, by.Length);
+                }
+                so.Close();
+                st.Close();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+
+    }
+}
---
+++ ConsoleApp2/WindowsFormsApp1/Form2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form2 : Form
+    {
+        public Form1 form1 { get; set; }
+        public Form2(Form1 nForm1)
+        {
+            form1 = nForm1;
+            InitializeComponent();
+        }
+
+
+        public Action<string, string, ProgressBar> Update;
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            Update = form1.DownloadFile;
+        }
+
+        public void UpdateUI(string URL, string filename)
+        {
+            Update?.Invoke(URL, filename, progressBar1);
+        }
+
+    }
+}
