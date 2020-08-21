44a191cc2b63c551f1b2a003e3759a4884244758
q361431349 <40018342+q361431349@users.noreply.github.com>
2020-08-21 17:40:54 +0800

    Add files via upload

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            CalculateResult(true);
+
+
+            await CalculateResult(false);
+
+
+
+
+            Console.ReadKey();
+        }
+
+        public static async Task CalculateResult(bool b)
+        {
+
+
+            var expensiveResultTask = Task.Run(async () =>
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    Console.WriteLine(i);
+                    if (b)
+                    {
+                        await Task.Delay(1000);
+                    }
+
+                }
+                Console.WriteLine();
+            });
+
+            await expensiveResultTask.ConfigureAwait(false);
+        }
+    }
+}
